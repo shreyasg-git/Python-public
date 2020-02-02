b83956ea355e61ab0338d3ebeb628173def27f5e
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2020-02-02 19:44:58 +0530

    Create Selection sort using python.py

---
+++ Selection sort using python.py
@@ -0,0 +1,24 @@
+nums=[]
+while True:                                   #loop2
+    num = input("Enter Number:- ")
+    if num.isnumeric():
+        nums.append(int(num))
+    elif num == 'done':
+        break
+print(nums)
+min=nums[0]
+for1count=0        #counter for for loop...but it always needs len(nums)-1 iterations.
+whilecount=0        #counter for while loop
+while whilecount<=len(nums):
+    for i in range(whilecount,len(nums)):
+        if nums[whilecount]>nums[i]:
+            #min=nums[i]
+            nums[i],nums[whilecount]=nums[whilecount],nums[i]
+            print(nums)
+        for1count+=1
+    whilecount+=1
+print('Lowest is',min)                #minimum number in the whole list
+print('for loop count',for1count)
+print('length',len(nums))
+print('whilecount',whilecount)
+print(nums)
