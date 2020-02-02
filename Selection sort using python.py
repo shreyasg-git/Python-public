fa80eaf19749ebf41f79e8f3e5f749dce31549ce
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2020-02-02 20:29:12 +0530

    Update Selection sort using python.py

--- Selection sort using python.py
+++ Selection sort using python.py
@@ -1,3 +1,5 @@
+#This only when user gives the input...
+#This can be implemented for random numbers also.
 nums=[]
 while True:                                   #loop2
     num = input("Enter Number:- ")
@@ -6,10 +8,10 @@ while True:                                   #loop2
     elif num == 'done':
         break
 print(nums)
-min=nums[0]
+#min=nums[0]
 for1count=0        #counter for for loop...but it always needs len(nums)-1 iterations.
 whilecount=0        #counter for while loop
-while whilecount<=len(nums):
+while whilecount<len(nums):
     for i in range(whilecount,len(nums)):
         if nums[whilecount]>nums[i]:
             #min=nums[i]
@@ -17,7 +19,7 @@ while whilecount<=len(nums):
             print(nums)
         for1count+=1
     whilecount+=1
-print('Lowest is',min)                #minimum number in the whole list
+#print('Lowest is',min)                #minimum number in the whole list
 print('for loop count',for1count)
 print('length',len(nums))
 print('whilecount',whilecount)
